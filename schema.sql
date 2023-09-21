fb984b0aab36d6d30ff499da91d5a9b44f085731
MHNazary <mhnazary2020@gmail.com>
2023-09-21 22:13:45 +0430

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,22 @@
+
+-- Add owner_id as a foreign key referencing owners(id)
+ALTER TABLE animals
+ADD COLUMN owner_id INTEGER;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_owner
+FOREIGN KEY (owner_id)
+REFERENCES owners (id);
+
+-- Add species_id as a foreign key referencing species(id)
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id)
+REFERENCES species (id);
+
 CREATE DATABASE vit_clinic;
 CREATE TABLE animals(
     id PRIMARY KEY INTEGER,
@@ -6,7 +25,21 @@ CREATE TABLE animals(
     escape_attempts INTEGER,
     neutered BOOLEAN,
     weight_kg DECIMAL(5,2),
+    owner_id INTEGER REFERENCES owners(id)
+    species_id INTEGER REFERENCES species(id),
 );
 -- Update the schema
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(50);
+
+
+CREATE TABLE owners (
+    id INTEGER PRIMARY KEY,
+    full_name VARCHAR(100),
+    age INTEGER
+);
+
+CREATE TABLE species (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(50)
+);
