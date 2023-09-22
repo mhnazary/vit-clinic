e3352ce230631243b53b712019870af45b3af1d8
MHNazary <mhnazary2020@gmail.com>
2023-09-22 21:19:03 +0430

    query number 6 and changed and added 8,9.

--- data.sql
+++ data.sql
@@ -59,24 +59,25 @@ INSERT INTO specializations (vet_id, species_id) VALUES
     (3, 1), 
     (4, 2); 
 
-INSERT INTO visits (animal_id, vet_id, visit_date) VALUES
-    (1, 1, '2020-05-24'),
-    (1, 3, '2020-07-22'),
-    (2, 4, '2021-02-02'),
-    (3, 2, '2020-01-05'),
-    (3, 2, '2020-03-08'),
-    (3, 2, '2020-05-14'),
-    (4, 3, '2021-05-04'),
-    (5, 4, '2021-02-24'),
-    (6, 2, '2019-12-21'),
-    (6, 1, '2020-08-10'),
-    (6, 2, '2021-04-07'),
-    (7, 3, '2019-09-29'),
-    (8, 4, '2020-10-03'),
-    (8, 4, '2020-11-04'),
-    (9, 2, '2019-01-24'),
-    (9, 2, '2019-05-15'),
-    (9, 2, '2020-02-27'),
-    (9, 2, '2020-08-03'),
-    (10, 3, '2020-05-24'), 
-    (10, 1, '2021-01-11'); 
+INSERT INTO visits (animal_id, vet_id, visit_date)
+VALUES
+    (1, 1, '2020-05-24'), -- Agumon visited William Tatcher
+    (1, 3, '2020-07-22'), -- Agumon also visited Stephanie Mendez
+    (2, 4, '2021-02-02'), -- Gabumon visited Jack Harkness
+    (3, 2, '2020-01-05'), -- Pikachu visited Maisy Smith
+    (3, 2, '2020-03-08'), -- Pikachu visited Maisy Smith again
+    (3, 2, '2020-05-14'), -- Pikachu visited Maisy Smith yet again
+    (4, 3, '2021-05-04'), -- Devimon visited Stephanie Mendez
+    (5, 4, '2021-02-24'), -- Charmander visited Jack Harkness
+    (6, 2, '2019-12-21'), -- Plantmon visited Maisy Smith
+    (6, 1, '2020-08-10'), -- Plantmon also visited William Tatcher
+    (6, 2, '2021-04-07'), -- Plantmon visited Maisy Smith again
+    (7, 3, '2019-09-29'), -- Squirtle visited Stephanie Mendez
+    (8, 4, '2020-10-03'), -- Angemon visited Jack Harkness
+    (8, 4, '2020-11-04'), -- Angemon visited Jack Harkness again
+    (9, 2, '2019-01-24'), -- Boarmon visited Maisy Smith
+    (9, 2, '2019-05-15'), -- Boarmon visited Maisy Smith again
+    (9, 2, '2020-02-27'), -- Boarmon visited Maisy Smith again
+    (9, 2, '2020-08-03'), -- Boarmon visited Maisy Smith again
+    (10, 3, '2020-05-24'), -- Blossom visited Stephanie Mendez
+    (10, 1, '2021-01-11'); -- Blossom also visited William Tatcher
--- queries.sql
+++ queries.sql
@@ -185,10 +185,8 @@ SELECT vet.name AS vet_name, MIN(v.visit_date) AS first_visit_date
 FROM visits v
 JOIN vets vet ON v.vet_id = vet.id
 JOIN animals a ON v.animal_id = a.id
-WHERE a.owner_id = (SELECT id FROM onwers WHERE full_name = 'Maisy Smith')
-GROUP BY vet.name
-ORDER BY first_visit_date
-LIMIT 1;
+WHERE vet.name = 'Vet Maisy Smith'
+GROUP BY vet.name;
 
 
 -- 7- Details for most recent visit: animal information, vet information, and date of visit.
@@ -213,7 +211,7 @@ FROM visits v
 JOIN animals a ON v.animal_id = a.id
 JOIN specializations sp ON a.species_id = sp.species_id
 JOIN species s ON sp.species_id = s.id
-WHERE a.owner_id = (SELECT id FROM owners WHERE full_name = 'Maisy Smith')
+WHERE a.owner_id = (SELECT id FROM onwers WHERE full_name = 'Maisy Smith')
 GROUP BY s.name
 ORDER BY visit_count DESC
 LIMIT 1;
