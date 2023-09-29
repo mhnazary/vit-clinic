819e8b78e3dfb700f381f91beb2e1b2b39f0cd9e
MHNazary <mhnazary2020@gmail.com>
2023-09-29 22:17:25 +0430

    Added index to create diagram

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -6,19 +6,19 @@ CREATE TABLE patients (
     date_of_birth DATE
 );
 
--- treatments table
-CREATE TABLE treatments (
-    id SERIAL PRIMARY KEY,
-    type VARCHAR(50),
-    name VARCHAR(80)
-);
-
 --medical_histories table
 CREATE TABLE medical_histories (
     id SERIAL PRIMARY KEY,
     admitted_at DATE,
     patient_id INT REFERENCES patients(id),
     status VARCHAR
+);
+
+-- treatments table
+CREATE TABLE treatments (
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(50),
+    name VARCHAR(80)
 );
 
 --invoices table
@@ -46,3 +46,10 @@ CREATE TABLE treatments_medical_histories (
     medical_history_id int REFERENCES medical_histories(id),
     PRIMARY KEY (treatment_id, medical_history_id)
 );
+
+CREATE INDEX idx_medical_histories_patient_id ON medical_histories(patient_id);
+CREATE INDEX idx_invoices_medical_history_id ON invoices(medical_history_id);
+CREATE INDEX idx_invoice_id ON invoice_items(invoice_id);
+CREATE INDEX idx_treatment_id ON invoice_items(treatment_id);
+CREATE INDEX idx_visits_treatment_id ON treatments_medical_histories(treatment_id);
+CREATE INDEX idx_visits_medical_history_id ON treatments_medical_histories(medical_history_id);
